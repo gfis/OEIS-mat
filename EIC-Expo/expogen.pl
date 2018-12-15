90730f0e82712e9705e8186dbad37f1bdf99be64
Georg Fischer <dr.Georg.Fischer@gmail.com>
2018-12-15 21:28:26 +0100

    renamed to EIC-Expo

--- EIC-Expo/expogen.pl
+++ EIC-Expo/expogen.pl
@@ -5,7 +5,7 @@
 # 2018-12-14, Georg Fischer
 #
 # usage:
-#   perl showgen.pl  [options] < Music1.en_de.txt
+#   perl expogen.pl  [options] < Music1.en_de.txt
 #       -d      debug level, 0 (none), 1 (some), 2 (more)
 #       -l      language: en, de
 #       -c      number of columns
@@ -66,64 +66,68 @@ GFis
 } # else ($action =~ m{^gen}) { # generate
 
 # print HTML header
-	open(HTM, ">", "web/index.$lang.html") or die "cannot write HTML file\n";
-	print HTM &get_html_head('Heidelberg 2019');
-	print HTM <<"GFis";
+    open(HTM, ">", "web/index.$lang.html") or die "cannot write HTML file\n";
+    print HTM &get_html_head('Heidelberg 2019');
+    print HTM <<"GFis";
 <body>
 <h3><a href="https://oeis.org" target="_blank">
 <img border="0" width="400" align="left" 
-	src="https://oeis.org/Luschny4.png" alt="On-Line Encyclopedia of Integer Sequences" title="OEIS" /></a>
+    src="https://oeis.org/Luschny4.png" alt="On-Line Encyclopedia of Integer Sequences" title="OEIS" /></a>
 GFis
-	print HTM &get_i18n(<<"GFis"
+    print HTM &get_i18n(<<"GFis"
 &nbsp;&nbsp;&nbsp;at the <em>Music and Math exhibit</em><br />
 GFis
-		, <<"GFis"
-&nbsp;&nbsp;&nbsp;in der Ausstellung <br />
-&nbsp;&nbsp;&nbsp;<em>Musik und Mathematik</em><br />
+        , <<"GFis"
+&nbsp;&nbsp;&nbsp;in der Ausstellung <em>Musik und Mathematik</em><br />
 GFis
-		);
-	print HTM <<"GFis";
+        );
+    my $lswitch = &lang_switch();
+    print HTM <<"GFis";
 &nbsp;&nbsp;&nbsp;Heidelberg 2019
 </h3>
+<p align="right" class="narrow">$lswitch</p>
 <table summary="Table of demo sequences">
 <tr>
 GFis
-	my $iseq = 0;
+    my $iseq = 0;
     while (<>) { # read the descriptions
         next if m{\A\s*\#}; # skip comments
         next if m{\A\s*\Z}; # skip empty lines
         s/\s+\Z//; # remove newline
         my $line = $_;
         if (0) { # switch for line types
-        } elsif ($line =~ m{^(A\d+)}) {
-        	my $aseqn = $1;
-        	if ($iseq > 0) { # not first
-        		if ($iseq % $ncols == 0) {
-        			print HTM "</td></tr>\n<!--****************-->\n<tr>\n";
-        		} else {
-        			print HTM "</td>\n";
-        		}
-        	} # not first
-        	$iseq ++;
-        	&proc_aseqn("xx", $aseqn);
+        } elsif ($line =~ m{^(A\d+)\s*(.*)}) {
+            my $aseqn      = $1;
+            my $extra_midi = $2;
+            $extra_midi =~ s{\s}{}g;   # remove all whitespac
+            $extra_midi =~ s{\,}{\&amp;}g; # & as http parameter sep.
+            if ($iseq > 0) { # not first
+                if ($iseq % $ncols == 0) {
+                    print HTM "</td></tr>\n<!--****************-->\n<tr>\n";
+                } else {
+                    print HTM "</td>\n";
+                }
+            } # not first
+            $iseq ++;
+            &proc_aseqn("xx", $aseqn, $extra_midi);
         } elsif ($line =~ m{^Title\:\s*(.*)}i) {
-        	&proc_title("en", &tag_a($1));
+            &proc_title("en", &tag_a($1));
         } elsif ($line =~ m{^Titel\:\s*(.*)}i) {
-        	&proc_title("de", &tag_a($1));
+            &proc_title("de", &tag_a($1));
         } elsif ($line =~ m{^Terms\:\s*(.*)}i) {
-        	&proc_terms("xx", &tag_a($1));
+            &proc_terms("xx", &tag_a($1));
         } elsif ($line =~ m{^Comment\:\s*(.*)}i) {
-        	&proc_commt("en", &tag_a($1));
+            &proc_commt("en", &tag_a($1));
         } elsif ($line =~ m{^Kommentar\:\s*(.*)}i) {
-        	&proc_commt("de", &tag_a($1));
+            &proc_commt("de", &tag_a($1));
         } elsif ($line =~ m{^Music\:\s*(.*)}i) {
-        	print HTM "<!-- Music: $1 -->\n";
+            print HTM "  <!-- Music: $1 -->\n";
         } else {
-        	# print STDERR "unkown line type: \"$line\"\n";
+            # print STDERR "unkown line type: \"$line\"\n";
         }
     } # while <>
 
-	print HTM <<"GFis";
+    print HTM <<"GFis";
 </td>
 <!--****************-->
 </tr>
@@ -131,93 +135,105 @@ GFis
 
 <br />
 GFis
-	print HTM &get_i18n(<<"GFis"
+    print HTM &get_i18n(<<"GFis"
 Links to <a href="https://oeis.org">OEIS</a> content are included according to the
 <a href="http://www.oeis.org/wiki/The_OEIS_End-User_License_Agreement">OEIS End-User License Agreement</a>.
 GFis
-		, <<"GFis"
+        , <<"GFis"
 Die Verweise auf die Inhalte der <a href="https://oeis.org">OEIS</a> unterliegen der
 <a href="http://www.oeis.org/wiki/The_OEIS_End-User_License_Agreement">OEIS Benutzer-Lizenz</a>.
 GFis
-		);
-	print HTM <<"GFis";
+        );
+    print HTM <<"GFis";
 
 <hr /><!--********************************-->
 <p class="comt"><em>For internal use:</em>
 </p>
-Generated at $TIMESTAMP by
-<a href="mailto:dr.georg.fischer\@gmail.com">Georg Fischer</a>
+Generated at $TIMESTAMP
+<br />Comments, questions? EMail <a href="mailto:dr.georg.fischer\@gmail.com">Georg Fischer</a>
+<br /><a href="https://github.com/gfis/OEIS-mat" target="_blank">Project on GitHub</a>
+<br /><a href="EIC-Expo.zip" >ZIP file</a> with all project material
+<br /><a href="Music1.en_de.txt" >Text file defining the example sequences</a>
 </body>
 </html>
 GFis
-	close(HTM);
-	exit(0);
+    close(HTM);
+    exit(0);
 # End of main
 #-------------------
+sub lang_switch { # HTML code for switching to other language
+    my  $result = <<"GFis"; # assume en -> de
+<a href="index.de.html" title="Switch to German" class="narrow">German Version</a>
+GFis
+    if ($lang eq "de") {
+        $result = <<"GFis";
+<a href="index.en.html" title="Umschalten auf Englisch" class="narrow">Englische Version</a>
+GFis
+    }
+    return $result;
+} # lang_switch
+#-------------------
 sub proc_aseqn { # start of cell
-	my ($plang, $aseqn) = @_;
-	print HTM "<td id=\"seq$iseq\" class=\"bor\"";
-	if ($iseq <= $ncols) {
-		print HTM " width=\"" . int(100 / $ncols) . "%\"";
-	}
-	print HTM ">"
-		. "<strong>"
-		. "<a href=\"https://oeis.org/search?q=id:$aseqn\&language=$languages{$lang}\" target=\"_blank\">"
-		. $aseqn
-		. "</a></strong>\n";
-		# the cell is continued by all other 'proc_*' subroutines
+    my ($plang, $aseqn, $extra_midi) = @_;
+    print HTM "<td id=\"seq$iseq\" class=\"bor\"";
+    if ($iseq <= $ncols) {
+        print HTM " width=\"" . int(100 / $ncols) . "%\"";
+    }
+    print HTM ">\n";
+    print HTM <<"GFis";
+  <strong><a href="https://oeis.org/search?q=id:$aseqn\&amp;language=$languages{$lang}"
+      target="_blank">$aseqn</a></strong>
+  <a href="https://oeis.org/play?seq=$aseqn\&amp;PLAY=PLAY\&amp;midi=1">
+    <img src="img/hear.jpg" title="Music from $aseqn" alt="Music from $aseqn" 
+      height="30" align="right"/></a>
+GFis
+    if ($extra_midi ne "") {
+        print HTM <<"GFis";
+  <a href="https://oeis.org/play?seq=$aseqn\&amp;PLAY=PLAY\&amp;midi=1\&amp;$extra_midi">
+    <img src="img/hear.jpg" title="Alternate music from $aseqn" alt="Alternate music from $aseqn" 
+      height="30" align="right"/></a>
+GFis
+    }
+    # https://oeis.org/play?seq=A322469&PLAY=PLAY&midi=1
+    # the cell is continued by all other 'proc_*' subroutines
 } # proc_aseqn
 #-------------------
 sub proc_title {
-	my ($plang, $text) = @_;
-	if ($plang eq $lang) {
-		print HTM "<br />$text\n";
-	}
+    my ($plang, $text) = @_;
+    if ($plang eq $lang) {
+        print HTM "  <br />$text\n";
+    }
 } # proc_title
 #-------------------
 sub proc_terms {
-	my ($plang, $text) = @_;
-	if (1 or $plang eq $lang) {
-		print HTM "<br />$text\n";
-	}
+    my ($plang, $text) = @_;
+    if (1 or $plang eq $lang) {
+        print HTM "  <br />$text\n";
+    }
 } # proc_terms
 #-------------------
 sub proc_commt {
-	my ($plang, $text) = @_;
-	if ($plang eq $lang) {
-		print HTM "<br />$text\n";
-	}
+    my ($plang, $text) = @_;
+    if ($plang eq $lang) {
+        print HTM "  <br /><span class=\"narrow\">$text</span>\n";
+    }
 } # proc_comte
 #----------------------
 sub tag_a { # replace [url anchor text] by <a href="url" target="_blank">anchor text</a>
-	my ($text) = @_;
-	while (($text =~ s{\[(\S+)\s+([^\]]+)\]}
-		              {\<a href\=\"\1\" target\=\"_blank\"\>\2\<\/a\>}) > 0) {
-	} # while replacing
-	return $text;
+    my ($text) = @_;
+    while (($text =~ s{\[(\S+)\s+([^\]]+)\]}
+                      {\<a href\=\"\1\" target\=\"_blank\"\>\2\<\/a\>}) > 0) {
+    } # while replacing
+    return $text;
 } # tag_a
 #----------------------
-sub wget {
-    my ($src_url, $tar_file) = @_;
-    my $sleep = 1;
-    if (! -r $tar_file) {
-        print "sleeping $sleep s before wget $tar_file\n";
-        sleep $sleep;
-        print `wget -o log.tmp -O $tar_file $src_url`;
-    }
-    my $buffer;
-    open(FIL, "<", $tar_file) or die "cannot read $tar_file\n";
-    read(FIL, $buffer, 100000000); # 100 MB
-    close(FIL);
-} # wget
-#-----------------------------
 sub get_i18n {
-	my @texts = @_;
-	my $result = $texts[0];
-	if ($lang eq "de") {
-		$result = $texts[1];
-	}
-	return $result;
+    my @texts = @_;
+    my $result = $texts[0];
+    if ($lang eq "de") {
+        $result = $texts[1];
+    }
+    return $result;
 }
 #-----------------------------
 sub get_html_head {
@@ -228,34 +244,22 @@ sub get_html_head {
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" [
 ]>
 <html xmlns="http://www.w3.org/1999/xhtml">
+<!-- generated at $TIMESTAMP
+    by https://github.com/gfis/OEIS-mat/blob/master/EIC-Expo/expogen.pl
+    Do not edit here!
+    
+    The ear icon is created by BAutomation and licensed with
+    Attribution-ShareAlike 4.0 International (CC BY-SA 4.0, 
+    https://creativecommons.org/licenses/by-sa/4.0/deed.en)
+    and stored at
+    https://commons.wikimedia.org/wiki/File:ListeningLS.jpg 
+-->
 <head>
-<title>$title</title>
-<meta name="generator" content="https://github.com/gfis/fasces/blob/master/oeis/database/subseq.pl" />
-<meta name="author"    content="Georg Fischer" />
-<style type="text/css">
-body,table,p,td,th
-        { font-family: Verdana,Arial,sans-serif; }
-table   { border-collapse: collapse; }
-td      { padding-right: 4px; }
-tr,td,th{ text-align: left; vertical-align:top; }
-.arr    { background-color: white          ; color: black; }
-.bor    { border-left  : 2px solid gray    ;
-          border-top   : 2px solid gray    ;
-          border-bottom: 2px solid gray    ;
-          border-right : 2px solid gray    ;                                 }
-.refp   { background-color: lightgreen;    } /* refers to the partner */
-.warn   { background-color: yellow;        } /* no reference to the partner and newer */
-.comt   { background-color: peachpuff;     } /* lightpink; #fff0f0; a lightred like in history, for conclusion */
-.err    { background-color: red; color: white; } /* for errors */
-.refn   { background-color: greenyellow;   } /* no reference to the partner */
-.more   { color:white  ; background-color: blue;      }
-.dead   { color:white  ; background-color: gray;      }
-.fini   { color:black  ; background-color: turquoise; }
-.narrow { font-size    : smaller;  }
-.prefor { font-family  : monospace; white-space: pre;
-          font-size    : large; font-weight: bold; }
-.same   { background-color:lightyellow }
-</style>
+    <title>$title</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link type="text/css" rel="stylesheet" href="stylesheet.css" />
+    <meta name="generator" content="https://github.com/gfis/OEIS-mat/blob/master/EIC-Expo/expogen.pl" />
+    <meta name="author"    content="Georg Fischer" />
 </head>
 GFis
 } # get_html_head
