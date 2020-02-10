3bf205bf4f7c1144132b4110eb351b187431ac12
Georg Fischer <dr.Georg.Fischer@gmail.com>
2020-02-10 10:12:16 +0100

    holrec.create combined

--- holrec/holrec.create.sql
+++ holrec/holrec.create.sql
@@ -1,5 +1,6 @@
 --  OEIS-mat/holrec/holrec.create.sql - holonomic recurrences
 --  @(#) $Id$
+--  2020-02-01: 2 tables combined
 --  2019-12-11, Georg Fischer
 --
 DROP    TABLE  IF EXISTS holrec;
@@ -7,11 +8,18 @@ CREATE  TABLE            holrec
     ( aseqno    VARCHAR(8)  NOT NULL  -- e.g. A322469
     , signature VARCHAR(1020)         -- comma separated, without "[ ]"
     , initterms VARCHAR(1020)         -- comma separated, without "[ ]"
-    , keyword   VARCHAR(64)           -- "guess,index,pass", "fail" ...
     , sigorder  INT                   -- number of signature elements, without const and a[n]
     , maxdegree INT                   -- 0 = linear
     , prefixno  INT                   -- number of terms prefixed to the recurrence
     , termpass  INT                   -- number of terms which passed
     , PRIMARY KEY(aseqno)
+    );
+COMMIT;
+--
+DROP    TABLE  IF EXISTS holatt;
+CREATE  TABLE            holatt
+    ( aseqno    VARCHAR(8)  NOT NULL  -- e.g. A322469 
+    , attr      VARCHAR(8)  NOT NULL  -- "guess", "index", "pass", "fail" ...
+    , PRIMARY KEY(aseqno, attr)
     );
 COMMIT;
