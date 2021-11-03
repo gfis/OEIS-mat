56a13ab07b7268319220258a2e58b66ad7f83f25
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-11-03 22:12:17 +0100

    dfsa.pl

--- contrib/rh/dfsa.pl
+++ contrib/rh/dfsa.pl
@@ -5,10 +5,13 @@
 # 2021-11-03, Georg Fischer
 #
 #:# Usage:
-#:#     perl dfsa.pl nsym "state_list"  > output
+#:#     perl dfsa.pl [-d mode] [-n noterms] nsym "state_list"  > output
+#:#         -d debugging mode
+#:#         -n number of terms to be computed
 #:#         input symbols are 0,1,2, ... nsym-1
 #:#         state_list is a string list of {0,1,2,3, ...}, 
 #:#             where 1 is the start state and 0 is the end (or "forbidden") state
+#:#     perl dfsa.pl -d 1 2 "1,2,3,2,3,0" | less # nx1 unimodal = A000124 Central polygonal numbers
 #--------------------------------------------------------
 use strict;
 use integer;
@@ -68,10 +71,10 @@ if (1) { # build all output strings of length n and count them
         $nstr{$isym} = 1;
     } # for $isym
     for (my $n = 1; $n < 64; ++$n) {
-    	if ($debug > 0) {
+        if ($debug > 0) {
             print "a($n) = " . scalar(keys %nstr) . "\n";
         } else {
-            print scalar(keys %nstr) . ",";
+            print scalar(keys %nstr) . "\n"; # ",";
         }
         %ostr = %nstr;
         %nstr = ();
@@ -96,9 +99,9 @@ sub compute {
         my $sym = substr($nstr, $istr, 1);
         $nstat = $table[$nstat][$sym];
         if ($debug >= 2) {
-        	print substr($nstr, 0, $istr) 
-        	    . "[>$nstat]"
-        	    . substr($nstr, $istr) . " ";
+            print substr($nstr, 0, $istr) 
+                . "[>$nstat]"
+                . substr($nstr, $istr) . " ";
         }
     }
     $nstat = $table[$nstat][$nsym];
@@ -110,3 +113,36 @@ sub compute {
 } # compute
 #--------------------------------------------
 __DATA__
+  state|   0   1 
+  -----+--------- 
+     s1|  s1  s2
+     s2|  s3  s2
+     s3|  s3  x0
+W
+
+       |  0 0   0 1   1 0   1 1   valid inputs
+  -----+-------------------------
+ 1 s1t1|  s1t1  s1t2  s2t1  s2t2  4
+ 2 s1t2|  s1t3  s1t2  s2t3  s2t2  4
+ 3 s1t3|  s1t3  s1x0  s2t3  s2x0  2
+ 4 s2t1|  s3t1  s3t2  s2t1  s2t2  4
+ 5 s2t2|  s3t3  s3t2  s2t3  s2t2  4
+ 6 s2t3|  s3t3  s3x0  s2t3  s2x0  2
+ 7 s3t1|  s3t1  s3t2  x0t1  x0t2  2
+ 8 s3t2|  s3t3  s3t2  x0t3  x0t2  2
+ 9 s3t3|  s3t3  s3x0  x0t3  x0x0  1
+
+
+    |  00 01 10 11  #vi
+  -----+-------------------------
+   1|  1  2  4  5    4
+   2|  3  2  6  5    4
+   3|  3  0  6  0    2
+   4|  7  8  4  5    4
+   5|  9  8  6  5    4
+   6|  9  0  6  0    2
+   7|  7  8  0  0    2
+   8|  9  8  0  0    2
+   9|  9  0  0  0    1
+
+perl dfsa.pl -d 1 4 "1,2,4,5,3,2,6,5,3,0,6,0,7,8,4,5,9,8,6,5,9,0,6,0,7,8,0,0,9,8,0,0,9,0,0,0"
