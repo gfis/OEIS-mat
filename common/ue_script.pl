e9e6efa806feede9bd954b802e4bb721f6797551
Georg Fischer <dr.georg.fischer@gamil.com>
2025-04-16 13:20:09 +0200

    close output window

--- common/ue_script.pl
+++ common/ue_script.pl
@@ -10,6 +10,7 @@ use Cwd qw(getcwd);
 
 my $sep = "\\\\";
 my $mode = shift(@ARGV);
+$mode =~ s{\W}{}g; # remove dash
 my $cwd = getcwd();
 $cwd =~ s{\s}{}g;
 $cwd =~ s{\/cygdrive\/c}{C\:};
@@ -23,8 +24,13 @@ GFis
 } elsif ($mode =~ m{open} ) {
     $call = <<"GFis";
 UltraEdit.open("$cwd${sep}joeis_names.txt");
-UltraEdit.activeDocument.readOnlyOn();
+UltraEdit.activeDocument.readOnlyOn(); 
+UltraEdit.outputWindow.showWindow(false);
 GFis
-}
-print        "$call";
+}                                                   
+my $file = "joeis_names_$mode.js";
+open(CAL, ">", $file) || die "cannot write \"$file\"\n";
+print CAL "$call";
+close(CAL);
+print `uedit64 /s=\"$cwd${sep}$file\"`;
 __DATA__
