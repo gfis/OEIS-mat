2b9c88640eddcbe52d07d3f480e7b06db0b404c2
Georg Fischer <dr.georg.fischer@gamil.com>
2026-09-10 16:00:40 +0200

    earlier.pl

--- contrib/lq/earlier.pl
+++ contrib/lq/earlier.pl
@@ -1,8 +1,8 @@
 #!perl
 
-# Preprocess "Number of earlier terms ..." 
+# Preprocess sequences with names "Number of earlier terms ..." of Leroy Quet
 # @(#) $Id$
-# 2026-03-11, V9.1: Pnnnnnn = DirectPredicate
+# 2026-09-10, Georg Fischer; *CH=50
 #:# Usage:
 #:#   perl earlier.pl in > out
 #--------------------------------------------------------
@@ -11,15 +11,19 @@ use integer;
 use warnings;
 
 while (<>) {
+    next if !m{^A};
+    s/\'//g;
     s/ \= /\=/g; 
     s/ *for n *\>\=? *\d+\, */ /i; 
     s/of the sequence//; 
     s/(such that|(which|that)( are)?) */ /; 
-    s/((is )?(the )?number of earlier terms */NET /; 
-    # A123456\ta(0)=1. a(n)=NET ,
-    s/[\.\;//g;
+    s/(is )?(the )?number of earlier terms */NET /; 
+    s/[\.\;]//g;
+    
     my $line = $_;
+    #A127463	a(0)=1 a(n)=NET a(k), 0<=k<=n-1,  (k+a(k)) is coprime to n
     #               1  1    2  2
-    $line =~ s/\ta\((\d)\)\=(\d) *a\(n\)NET */\tmultia\t$1\t\"$2\"\t\(self, n\) \-\> CNT\(\t/;
-    print $line;
+    $line =~ s{\ta\((\d)\)\=(\d) *a\(n\)[ \=]NET *}
+              {\tmultia\t$1\t\"$2\"\t\(self, n\) \-\> CNT\($1\, n \- 1\, k \-\> Predicates..is(self.a(k)))\t};
+    print "$line";
 }
